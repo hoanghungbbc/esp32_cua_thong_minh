6c0d5ebbef23f86dcc6fd3c151bfca58ea9d6430
hoanghungbbc <hoanghung002004@gmail.com>
2026-05-20 11:48:04 +0700

    Integrate WiFiManager and improve card handling
    Add WiFiManager for WiFi configuration and enhance card management features.

--- testDATN.ino
+++ testDATN.ino
@@ -10,12 +10,10 @@
 #include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
 #include <Preferences.h>
-
+#include <WiFiManager.h>
 #include <Keypad.h>
 #include <Adafruit_Fingerprint.h>
 
-char ssid[] = "hu.g";
-char pass[] = "135792468";
 #define BUZZER 2
 #define SS_PIN 5
 #define RST_PIN 4
@@ -24,15 +22,22 @@ char pass[] = "135792468";
 MFRC522 rfid(SS_PIN, RST_PIN);
 Servo myservo;
 Preferences preferences;
-
 LiquidCrystal_I2C lcd(0x27,16,2);
+WiFiManager wm;
+
+String wifiPass = "AAAA";
 
 String cardUID[20];
-int cardCount = 0;
+int cardID[20];   // thêm mảng ID
+int cardCount;
+//int nextCardID = 1;
+
 int nextFingerID = 1;
+
 bool addMode = false;
 bool deleteMode = false;
-
+bool enterDeleteID = false;
+String deleteIDInput = "";
 /* ------------ Fingerprint ------------- */
 
 HardwareSerial mySerial(2);
@@ -59,104 +64,145 @@ Keypad keypad = Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
 String password="1111";
 String input="";
 
+/* ---------- FUNCTION DECLARE ---------- */
+
+void beep(int timeDelay);
+bool checkWiFiPassword();
+void resetWiFi();
+
 /* ---------- BLYNK ---------- */
 
 BLYNK_WRITE(V3)
 {
-int v = param.asInt();
-if(v == 1)
-{
-deleteMode = true;
-lcd.clear();
-lcd.print("Quet the xoa");
-}
-}
+  int v = param.asInt();
 
+  if(v == 1)
+  {
+    enterDeleteID = true;
+    deleteIDInput = "";
+
+    lcd.clear();
+    lcd.print("Nhap ID xoa");
+  }
+}
 BLYNK_WRITE(V0)
 {
-int value = param.asInt();
+  int value = param.asInt();
 
-if(value == 1)
-{
-myservo.write(90);
-lcd.clear();
-lcd.print("mo cua");
-}
-else
-{
-myservo.write(0);
-lcd.clear();
-lcd.print("dong cua");
-}
+  if(value == 1)
+  {
+    myservo.write(90);
+
+    lcd.clear();
+    lcd.print("mo cua");
+  }
+  else
+  {
+    myservo.write(0);
+
+    lcd.clear();
+    lcd.print("dong cua");
+  }
 }
 
 BLYNK_WRITE(V2)
 {
-int v = param.asInt();
+  int v = param.asInt();
 
-if(v == 1)
-{
-addMode = true;
-lcd.clear();
-lcd.print("them the moi");
-}
+  if(v == 1)
+  {
+    addMode = true;
+
+    lcd.clear();
+    lcd.print("them the moi");
+  }
 }
 
 /* ---------- RFID ---------- */
 
 bool checkCard(String uid)
 {
-for(int i=0;i<cardCount;i++)
-{
-if(uid == cardUID[i]) return true;
-}
-return false;
+  for(int i=0;i<cardCount;i++)
+  {
+    if(uid == cardUID[i]) return true;
+  }
+
+  return false;
 }
 
 void saveCards()
 {
-preferences.putInt("count", cardCount);
+  preferences.putInt("count", cardCount);
 
-for(int i=0;i<cardCount;i++)
+  for(int i=0;i<cardCount;i++)
+  {
+    preferences.putString(("uid"+String(i)).c_str(), cardUID[i]);
+    preferences.putInt(("id"+String(i)).c_str(), cardID[i]);
+  }
+}
+void deleteCardByID(int id)
 {
-String key = "card" + String(i);
-preferences.putString(key.c_str(), cardUID[i]);
-}
-}
+  for(int i=0;i<cardCount;i++)
+  {
+    if(cardID[i] == id)
+    {
+      for(int j=i;j<cardCount-1;j++)
+      {
+        cardUID[j] = cardUID[j+1];
+        cardID[j]  = cardID[j+1];
+      }
 
+      cardCount--;
+
+      saveCards();
+
+      lcd.clear();
+      lcd.print("Da xoa ID:");
+      lcd.print(id);
+
+      delay(2000);
+      return;
+    }
+  }
+
+  lcd.clear();
+  lcd.print("Khong tim thay");
+  delay(2000);
+}
 void loadCards()
 {
-cardCount = preferences.getInt("count",0);
+  cardCount = preferences.getInt("count",0);
 
-for(int i=0;i<cardCount;i++)
-{
-String key = "card" + String(i);
-cardUID[i] = preferences.getString(key.c_str(),"");
-}
-}
-
-void deleteCard(String uid)
-{
-for(int i=0;i<cardCount;i++)
-{
-if(cardUID[i] == uid)
-{
-for(int j=i;j<cardCount-1;j++)
-{
-cardUID[j] = cardUID[j+1];
+  for(int i=0;i<cardCount;i++)
+  {
+    cardUID[i] = preferences.getString(("uid"+String(i)).c_str(),"");
+    cardID[i]  = preferences.getInt(("id"+String(i)).c_str(),0);
+  }
 }
 
-cardCount--;
-saveCards();
 
-lcd.clear();
-lcd.print("Da xoa the");
-return;
-}
-}
+int getAvailableID()
+{
+  for(int id = 1; id <= 20; id++)
+  {
+    bool used = false;
 
-lcd.clear();
-lcd.print("Khong tim thay");
+    for(int i = 0; i < cardCount; i++)
+    {
+      if(cardID[i] == id)
+      {
+        used = true;
+        break;
+      }
+    }
+
+    if(!used)
+    {
+      return id;
+    }
+  }
+
+  return -1;
 }
 void saveFingerID()
 {
@@ -167,331 +213,605 @@ void loadFingerID()
 {
   nextFingerID = preferences.getInt("fid",1);
 }
+
 /* ---------- Servo ---------- */
 
 void openDoor()
 {
-myservo.write(90);
-lcd.clear();
-lcd.print("Mo cua");
-delay(3000);
-myservo.write(0);
-lcd.clear();
-lcd.print("Dong cua");
+  myservo.write(90);
+
+  lcd.clear();
+  lcd.print("Mo cua");
+
+  delay(3000);
+
+  myservo.write(0);
+
+  lcd.clear();
+  lcd.print("Dong cua");
 }
 
 /* ---------- Fingerprint ---------- */
+
 void deleteAllFingerprint()
 {
-lcd.clear();
-lcd.print("Dang xoa...");
+  lcd.clear();
+  lcd.print("Dang xoa...");
 
-uint8_t p = finger.emptyDatabase();
+  uint8_t p = finger.emptyDatabase();
 
-if(p == FINGERPRINT_OK)
-{
-lcd.clear();
-lcd.print("Da xoa tat ca");
+  if(p == FINGERPRINT_OK)
+  {
+    lcd.clear();
+    lcd.print("Da xoa tat ca");
 
-nextFingerID = 1;     // reset ID vân tay
-saveFingerID();       // lưu lại
+    nextFingerID = 1;
 
-delay(2000);
+    saveFingerID();
+
+    delay(1000);
+  }
+  else
+  {
+    lcd.clear();
+    lcd.print("Xoa that bai");
+
+    delay(1000);
+  }
 }
-else
-{
-lcd.clear();
-lcd.print("Xoa that bai");
-delay(2000);
-}
-}
+
 void addFingerprint()
 {
-int id = nextFingerID;
+  int id = nextFingerID;
 
-lcd.clear();
-lcd.print("ID moi:");
-lcd.setCursor(0,1);
-lcd.print(id);
+  lcd.clear();
+  lcd.print("ID moi:");
 
-delay(2000);
+  lcd.setCursor(0,1);
+  lcd.print(id);
 
-lcd.clear();
-lcd.print("Dat van tay");
+  delay(1000);
 
-while(finger.getImage()!=FINGERPRINT_OK);
+  lcd.clear();
+  lcd.print("Dat van tay");
 
-finger.image2Tz(1);
+  while(finger.getImage()!=FINGERPRINT_OK);
 
-lcd.clear();
-lcd.print("Dat lan 2");
+  finger.image2Tz(1);
 
-delay(2000);
+  lcd.clear();
+  lcd.print("Dat lan 2");
 
-while(finger.getImage()!=FINGERPRINT_OK);
+  delay(1000);
 
-finger.image2Tz(2);
+  while(finger.getImage()!=FINGERPRINT_OK);
 
-if(finger.createModel()==FINGERPRINT_OK)
-{
-finger.storeModel(id);
+  finger.image2Tz(2);
 
-lcd.clear();
-lcd.print("Da them ID:");
-lcd.setCursor(0,1);
-lcd.print(id);
+  if(finger.createModel()==FINGERPRINT_OK)
+  {
+    finger.storeModel(id);
 
-nextFingerID++;     // tăng ID
-saveFingerID();     // lưu lại
+    lcd.clear();
+    lcd.print("Da them ID:");
 
-delay(2000);
+    lcd.setCursor(0,1);
+    lcd.print(id);
+
+    nextFingerID++;
+
+    saveFingerID();
+
+    delay(2000);
+  }
+  else
+  {
+    lcd.clear();
+    lcd.print("Them that bai");
+
+    delay(1000);
+  }
 }
-else
-{
-lcd.clear();
-lcd.print("Them that bai");
-delay(2000);
-}
-}
-
 void deleteFingerprint()
 {
-lcd.clear();
-lcd.print("Nhap ID xoa");
+  lcd.clear();
+  lcd.print("Nhap ID xoa");
 
-String id="";
+  String id="";
 
-while(true)
-{
-char k=keypad.getKey();
+  while(true)
+  {
+    char k=keypad.getKey();
 
-if(k)
-{
-if(k=='#')
-{
-finger.deleteModel(id.toInt());
+    if(k)
+    {
+      if(k=='#')
+      {
+        finger.deleteModel(id.toInt());
 
-lcd.clear();
-lcd.print("Da xoa VT");
-delay(2000);
-break;
-}
-else
-{
-id+=k;
-lcd.setCursor(0,1);
-lcd.print(id);
-}
-}
-}
+        lcd.clear();
+        lcd.print("Da xoa VT");
+
+        delay(1000);
+
+        break;
+      }
+      else
+      {
+        id+=k;
+
+        lcd.setCursor(0,1);
+        lcd.print(id);
+      }
+    }
+  }
 }
 
 void checkFingerprint()
 {
-if(finger.getImage()!=FINGERPRINT_OK){
-  return;
-} 
+  if(finger.getImage()!=FINGERPRINT_OK)
+  {
+    return;
+  }
 
-finger.image2Tz();
+  finger.image2Tz();
 
-if(finger.fingerFastSearch()==FINGERPRINT_OK)
-{
-lcd.clear();
-lcd.print("Van tay dung ID:");
-lcd.setCursor(0,1);
-lcd.print(finger.fingerID);
-delay(500);
-openDoor();
-}
-else
-{
-lcd.clear();
-lcd.print("VT sai");
+  if(finger.fingerFastSearch()==FINGERPRINT_OK)
+  {
+    lcd.clear();
+    lcd.print("Van tay dung ID:");
 
-beep(200);
-delay(200);
-beep(200);   // beep 3 lần báo sai
-delay(200);
-beep(200);
-lcd.clear();
-lcd.print("Dong cua");
-delay(1500);
-}
+    lcd.setCursor(0,1);
+    lcd.print(finger.fingerID);
+
+    delay(500);
+
+    openDoor();
+  }
+  else
+  {
+    lcd.clear();
+    lcd.print("VT sai");
+
+    beep(200);
+    delay(200);
+
+    beep(200);
+    delay(200);
+
+    beep(200);
+
+    lcd.clear();
+    lcd.print("Dong cua");
+
+    delay(1000);
+  }
 }
 
 /* ---------- Keypad ---------- */
 
 bool checkPassword()
 {
-lcd.clear();
-lcd.print("Nhap pass");
+  lcd.clear();
+  lcd.print("Nhap pass");
 
-input="";
+  input="";
 
-while(true)
-{
-char key = keypad.getKey();
+  while(true)
+  {
+    char key = keypad.getKey();
 
-if(key)
-{
-if(key=='#')
-{
-if(input==password)
-{
-lcd.clear();
-lcd.print("Pass dung");
-delay(1000);
-return true;
-}
-else
-{
-lcd.clear();
-lcd.print("Sai pass");
-delay(1500);
-return false;
-}
-}
-else
-{
-input+=key;
-lcd.setCursor(0,1);
-lcd.print(input);
-}
-}
-}
-}
+    if(key)
+    {
+      if(key=='#')
+      {
+        if(input==password)
+        {
+          lcd.clear();
+          lcd.print("Pass dung");
+          return true;
+        }
+        else
+        {
+          lcd.clear();
+          lcd.print("Sai pass");
+          return false;
+        }
+      }
+      else
+      {
+        input+=key;
 
+        lcd.setCursor(0,1);
+        lcd.print(input);
+      }
+    }
+  }
+}
 void keypadControl()
 {
-char key = keypad.getKey();
+  char key = keypad.getKey();
 
-if(key=='*')
-{
-if(checkPassword())
-{
-addFingerprint();
+  // ===== MỞ CỬA BẰNG MẬT KHẨU =====
+  if(key=='D')
+  {
+    if(checkPassword())
+    {
+      lcd.clear();
+      lcd.print("Mo bang MK");
+      openDoor();
+    }
+    else
+    {
+      lcd.clear();
+      lcd.print("Sai MK");
+
+      beep(200);
+      delay(200);
+      beep(200);
+    }
+  }
+
+  // ===== ĐỔI MẬT KHẨU =====
+  if(key=='#')
+  {
+    changePassword();
+  }
+
+  if(key=='*')
+  {
+    if(checkPassword())
+    {
+      addFingerprint();
+    }
+  }
+
+  if(key=='C')
+  {
+    if(checkPassword())
+    {
+      deleteAllFingerprint();
+    }
+  }
+
+  if(key=='B')
+  {
+    if(checkPassword())
+    {
+      deleteFingerprint();
+    }
+  }
+
+  if(key=='A')
+  {
+    if(checkWiFiPassword())
+    {
+      resetWiFi();
+    }
+  }
 }
-}
-if(key=='C')     // xoa tat ca van tay
-{
-if(checkPassword())
-{
-deleteAllFingerprint();
-}
-}
-if(key=='B')
-{
-if(checkPassword())
-{
-deleteFingerprint();
-}
-}
-}
+
+/* ---------- Buzzer ---------- */
+
 void beep(int timeDelay)
 {
-digitalWrite(BUZZER, HIGH);
-delay(timeDelay);
-digitalWrite(BUZZER, LOW);
-}
-/* ---------- SETUP ---------- */
+  digitalWrite(BUZZER, HIGH);
 
+  delay(timeDelay);
+
+  digitalWrite(BUZZER, LOW);
+}
+
+/* ---------- WiFi ---------- */
+
+bool checkWiFiPassword()
+{
+  lcd.clear();
+  lcd.print("Pass WiFi:");
+
+  String input="";
+
+  while(true)
+  {
+    char key = keypad.getKey();
+
+    if(key)
+    {
+      if(key=='#')
+      {
+        if(input == wifiPass)
+        {
+          lcd.clear();
+          lcd.print("Dung MK");
+
+          delay(500);
+
+          return true;
+        }
+        else
+        {
+          lcd.clear();
+          lcd.print("Sai MK");
+
+          delay(500);
+
+          return false;
+        }
+      }
+      else
+      {
+        input += key;
+
+        lcd.setCursor(0,1);
+        lcd.print(input);
+      }
+    }
+  }
+}
+
+void resetWiFi()
+{
+  lcd.clear();
+  lcd.print("Reset WiFi");
+
+  wm.resetSettings();
+
+  delay(1000);
+
+  lcd.clear();
+  lcd.print("Nhap WiFi moi");
+
+  wm.startConfigPortal("ESP32_KhoaCua");
+
+  lcd.clear();
+  lcd.print("Done!");
+
+  delay(500);
+
+  ESP.restart();
+}
+
+/* ---------- SETUP ---------- */
+void savePassword(String newPass)
+{
+  preferences.putString("pass", newPass);
+}
+void changePassword()
+{
+  lcd.clear();
+  lcd.print("Pass cu:");
+
+  String oldPass="";
+
+  // nhập mật khẩu cũ
+  while(true)
+  {
+    char k = keypad.getKey();
+
+    if(k)
+    {
+      if(k=='#')
+      {
+        if(oldPass == password)
+        {
+          break;
+        }
+        else
+        {
+          lcd.clear();
+          lcd.print("Sai pass");
+          delay(1000);
+          return;
+        }
+      }
+      else
+      {
+        oldPass += k;
+        lcd.setCursor(0,1);
+        lcd.print(oldPass);
+      }
+    }
+  }
+
+  // nhập mật khẩu mới
+  lcd.clear();
+  lcd.print("Pass moi:");
+
+  String newPass="";
+
+  while(true)
+  {
+    char k = keypad.getKey();
+
+    if(k)
+    {
+      if(k=='#')
+      {
+        password = newPass;
+        savePassword(newPass);
+
+        lcd.clear();
+        lcd.print("Da doi MK");
+
+        delay(1000);
+        return;
+      }
+      else
+      {
+        newPass += k;
+        lcd.setCursor(0,1);
+        lcd.print(newPass);
+      }
+    }
+  }
+}
 void setup()
 {
   pinMode(BUZZER, OUTPUT);
-digitalWrite(BUZZER, LOW);
-Serial.begin(115200);
 
-SPI.begin();
-rfid.PCD_Init();
+  digitalWrite(BUZZER, LOW);
 
-myservo.attach(SERVO_PIN);
-myservo.write(0);
+  Serial.begin(115200);
 
-preferences.begin("rfid", false);
-loadCards();
+  SPI.begin();
+  rfid.PCD_Init();
 
-Wire.begin(21,22);
+  myservo.attach(SERVO_PIN);
+  myservo.write(0);
 
-lcd.init();
-lcd.backlight();
+  preferences.begin("rfid", false);
+password = preferences.getString("pass", "1111");
+  loadCards();
+  loadFingerID();
 
-lcd.print("System Ready");
+  Wire.begin(21,22);
 
-mySerial.begin(57600,SERIAL_8N1,16,17);
-finger.begin(57600);
+  lcd.init();
+  lcd.backlight();
 
-Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  lcd.print("System Ready");
+
+  mySerial.begin(57600,SERIAL_8N1,16,17);
+
+  finger.begin(57600);
+
+  bool res = wm.autoConnect("ESP32_KhoaCua");
+
+  if(!res)
+  {
+    lcd.clear();
+    lcd.print("Config WiFi");
+
+    wm.startConfigPortal("ESP32_KhoaCua");
+  }
+
+  lcd.clear();
+  lcd.print("WiFi OK");
+
+  Blynk.begin(BLYNK_AUTH_TOKEN,
+              WiFi.SSID().c_str(),
+              WiFi.psk().c_str());
 }
 
 /* ---------- LOOP ---------- */
 
 void loop()
 {
-Blynk.run();
+  Blynk.run();
 
-keypadControl();
-checkFingerprint();
+  keypadControl();
 
-if (!rfid.PICC_IsNewCardPresent()) return;
-if (!rfid.PICC_ReadCardSerial()) return;
+  checkFingerprint();
 
-String uid="";
+if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
+{
+  String uid="";
 
 for(byte i=0;i<rfid.uid.size;i++)
 {
-uid += String(rfid.uid.uidByte[i],HEX);
+  if(rfid.uid.uidByte[i] < 0x10)
+  {
+    uid += "0";
+  }
+
+  uid += String(rfid.uid.uidByte[i], HEX);
 }
 
-if(addMode)
-{
-if(checkCard(uid))   // đã tồn tại
+  uid.toUpperCase();
+
+  if(addMode)
+  {
+    if(checkCard(uid))
+    {
+      lcd.clear();
+      lcd.print("The da ton tai");
+      delay(1000);
+    }
+    else
+    {
+      if(cardCount < 20)
+      {
+        cardUID[cardCount] = uid;
+int newID = getAvailableID();
+
+if(newID == -1)
 {
   lcd.clear();
-  lcd.print("The da ton tai");
-  delay(1500);
+  lcd.print("Het ID");
+  return;
 }
-else
-{
-  if(cardCount < 3)//tran bo nho----------
-  {
-    cardUID[cardCount++] = uid;
-    saveCards();
 
-    lcd.clear();
-    lcd.print("Da them the");
+cardID[cardCount] = newID;
+
+lcd.clear();
+lcd.print("ID:");
+lcd.print(newID);
+
+cardCount++;
+
+        saveCards();
+        delay(1000);
+      }
+      else
+      {
+        lcd.clear();
+        lcd.print("Full bo nho");
+      }
+    }
+
+    addMode = false;
   }
   else
   {
-    lcd.clear();
-    lcd.print("Full bo nho");
+    if(checkCard(uid))
+    {
+      for(int i=0;i<cardCount;i++)
+      {
+        if(cardUID[i] == uid)
+        {
+          lcd.clear();
+          lcd.print("ID:");
+          lcd.print(cardID[i]);
+          delay(500);
+          break;
+        }
+      }
+
+      openDoor();
+    }
+    else
+    {
+      lcd.clear();
+      lcd.print("The khong hop le");
+      delay(1500);
+
+      lcd.clear();
+      lcd.print("Dong cua");
+      delay(500);
+    }
+  }
+
+  rfid.PICC_HaltA();
+}
+  if(enterDeleteID)
+{
+  char k = keypad.getKey();
+
+  if(k)
+  {
+    if(k=='#')
+    {
+      deleteCardByID(deleteIDInput.toInt());
+      enterDeleteID = false;
+    }
+    else
+    {
+      deleteIDInput += k;
+      lcd.setCursor(0,1);
+      lcd.print(deleteIDInput);
+    }
   }
 }
 
-addMode = false;
-}
 
-else if(deleteMode)
-{
-deleteCard(uid);
-deleteMode=false;
-}
-
-else
-{
-if(checkCard(uid))
-{
-openDoor();
-}
-else
-{
-lcd.clear();
-lcd.print("The khong hop le");
-delay(1500);
-lcd.clear();
-lcd.print("Dong cua");
-delay(1500);
-}
-}
-
-rfid.PICC_HaltA();
 }
